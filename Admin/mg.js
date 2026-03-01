ed6953d2ec47e40cf322a24bee3aa5ac291e0170
maddiegee759 <maddie.gee.759@gmail.com>
2026-03-01 19:44:53 +0000

    adding js to blog post

---
+++ Admin/mg.js
@@ -0,0 +1,16 @@
+let newPost = document.getElementById("newPost");
+
+newPost.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let editor = document.getElementById("editor");
+    let title = document.getElementById("title");
+    let author = document.getElementById("author");
+    let content = document.getElementById("content");
+    let photo = document.getElementById("photo");
+
+    alert("New post submitted!");
+    console.log(
+      `This form has an editor of ${editor.value}, title of ${title.value}, author(s) of ${author.value}, content of ${content.value}, and photo URL of ${photo.value}`
+    );
+});
