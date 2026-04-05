3d0c350cdeb78ffa73e27d339ed694a59e5761ff
maddiegee759 <maddie.gee.759@gmail.com>
2026-04-05 17:24:20 +0100

    edit post stuff

--- adminpages/newPost.js
+++ adminpages/newPost.js
@@ -10,44 +10,8 @@ newPost.addEventListener("submit", function(event) {
     let content = document.getElementById("content");
     let photo = document.getElementById("photo");
 
-    alert(`New post submitted! This form has an editor of ${editor.value}, title of ${title.value}, author(s) of ${author.value}, content of ${content.value}`);
+    alert(`New post submitted! This form has an editor of ${editor.value}, title of ${title.value}, author(s) of ${author.value}, content of ${content.value}, and photo of ${photo.value}`);
     console.log(
-      `This form has an editor of ${editor.value}, title of ${title.value}, author(s) of ${author.value}, content of ${content.value}`
+      `This form has an editor of ${editor.value}, title of ${title.value}, author(s) of ${author.value}, content of ${content.value}, and photo of ${photo.value}`
     );
 });
-
-//https://gist.github.com/MirzaLeka/db9386c400fd75edd99f6d5162728f7f
-function upload() {
-  const fileUploadInput = document.querySelector('.file-uploader');
-
-  /// Validations ///
-
-  if (!fileUploadInput.value) {
-    return;
-  }
-
-  // using index [0] to take the first file from the array
-  const image = fileUploadInput.files[0];
-
-  // check if the file selected is not an image file
-  if (!image.type.includes('image')) {
-    return alert('Only images are allowed!');
-  }
-
-  // check if size (in bytes) exceeds 10 MB
-  if (image.size > 10_000_000) {
-    return alert('Maximum upload size is 10MB!');
-  }
-
-  /// Display the image on the screen ///
-
-  const fileReader = new FileReader();
-  fileReader.readAsDataURL(image);
-
-  fileReader.onload = (fileReaderEvent) => {
-    const profilePicture = document.querySelector('.profile-picture');
-    profilePicture.style.backgroundImage = `url(${fileReaderEvent.target.result})`;
-  }
-
-  // upload image to the server or the cloud
-}
