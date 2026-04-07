e052e2ebae8f6438e8f13304b64f36fb20853bda
maddiegee759 <maddie.gee.759@gmail.com>
2026-04-07 15:12:15 +0100

    login copy paste

---
+++ adminpages/adminLogin.js
@@ -0,0 +1,40 @@
+document.getElementById("login").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const data = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+    };
+
+    try {
+        const response = await fetch(`${URL}/api/adminLogin`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        if (response.ok) 
+            
+        {
+            alert("Logged In success");
+
+            //saves logged in user - not secure but not sure how else to do this tbh
+            localStorage.setItem("userId", result.user.id);
+
+            document.getElementById("login").reset();
+            window.location.href = "adminhome.html"
+        } 
+        else 
+        {
+            alert("Login Failed");
+        }
+
+    } catch (error) {
+        console.error(error);
+        alert("UH OH.");
+    }
+});
