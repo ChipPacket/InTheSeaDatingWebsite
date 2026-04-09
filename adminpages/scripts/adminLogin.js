7d40394f195aaf30df5f64dcce0f440fdbea6e99
maddiegee759 <maddie.gee.759@gmail.com>
2026-04-09 19:43:32 +0100

    admin login incl password

--- adminpages/scripts/adminLogin.js
+++ adminpages/scripts/adminLogin.js
@@ -1,13 +1,16 @@
+const KEY = "3441462ecb5c1866294702d261de3d861d4636786208992d212a702233267367";
+const URL = "http://127.0.0.1:5000";
+
 document.getElementById("login").addEventListener("submit", async function(e) {
     e.preventDefault();
 
     const data = {
-        email: document.getElementById("email").value,
+        adminId: document.getElementById("adminId").value,
         password: document.getElementById("password").value,
     };
 
     try {
-        const response = await fetch(`${URL}/api/adminLogin`, {
+        const response = await fetch(`${URL}/api/admin/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
