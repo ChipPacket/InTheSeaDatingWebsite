fc3951229eb379496ec1c109a98e1552bcaa5ae5
carysblyth <carysblyth@gmail.com>
2026-04-10 16:38:50 +0100

    navbar log in fixed

---
+++ clientpages/scripts/checklogin.js
@@ -0,0 +1,23 @@
+
+function checkUserLogIn(){
+    //check if user is logged in thus userID exists
+    const imageid = localStorage.getItem("userId");
+    // if (localStorage.getItem("userId")!==null){
+        // enable links
+    if (imageid){
+        document.getElementById("findMyCatch").href = "findyourcatch.html";
+
+        // change sign up
+        document.getElementById("myProfile").href = "profile.html";
+
+        // change log in -> myprofile
+        document.getElementById("opt1").innerHTML = "My Profile";
+        document.getElementById("opt1").href = "profile.html";
+
+        // change sign up -> log out
+        document.getElementById("opt2").innerHTML = "Log Out";
+        document.getElementById("opt2").onclick = "logout()";
+    }
+}
+
+checkUserLogIn();
